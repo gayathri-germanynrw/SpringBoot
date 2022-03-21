7c52f8dbee76ce3973419a6b2f9b22400c658bfe
gayathri-germanynrw <gayathrikasi@gmail.com>
2022-03-21 11:04:33 +0100

    Apache Kafka Producer commit

---
+++ spring-boot-kafka-producer/src/main/java/com/gkasi/kafka/HelloController.java
@@ -0,0 +1,24 @@
+package com.gkasi.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello/{message}")
+public class HelloController {
+	
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
+	
+	//publish message through Kafka Template
+	public String publish(@PathVariable("message") String publishMessage) {
+		kafkaTemplate.send("helloTopic", publishMessage);
+		return  "Message Published :"+ publishMessage;
+		
+
+	}
+
+}
